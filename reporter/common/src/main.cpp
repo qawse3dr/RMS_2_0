1a5895cffabdfe61c296f8dda5bd530d8efa0013
qawse3dr <lawrencemilne38@gmail.com>
2021-09-14 21:41:43 -0400

    log Request Reporter #18
    Created log reporter and converted reporting to a class

--- reporter/common/src/main.cpp
+++ reporter/common/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "common/consumer/cpu_consumer.h"
 #include "common/consumer/ram_consumer.h"
+#include "rms_common/request_client.h"
 
 //TODO delete
 #include "rms_common/request_data.h"
@@ -19,16 +20,23 @@ int main() {
     rms::reporter::CpuConsumer cpu_consumer(4);
     rms::reporter::RamConsumer ram_consumer;
 
+
+    //start request_client_
+    rms::common::request_client_.start();
+    //Start Consumers
     cpu_consumer.start();
     ram_consumer.start();
 
 
-    //std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 
-    rms::common::pollRequests();
+    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+
+    
 
     cpu_consumer.stop();
     ram_consumer.stop();
+
+    rms::common::request_client_.stop();
 
     std::cout << "sizeOf RequestData " << sizeof(struct rms::common::RequestData) << std::endl;
     return 0;
